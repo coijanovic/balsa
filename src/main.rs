c1f765401a16a8a7d45ee36def9228e173ee85ae
Christoph Coijanovic <christoph.coijanovic@kit.edu>
2022-12-19 09:44:03 +0100

    output working!

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ extern crate clap;
 
 use clap::Parser;
 
-const EMOJI: [Emoji; 2] = [
+const EMOJI: [Emoji; 5] = [
     Emoji{color: (255,255,255), value: '🌝'},
-    Emoji{color: (0,0,0), value: '🌚'},
+    Emoji{color: (0,0,0), value: '🖤'},
+    Emoji{color: (255,0,0), value: '😡'},
+    Emoji{color: (0,255,0), value: '🤢'},
+    Emoji{color: (0,0,255), value: '🌊'},
 ];
 
 /// Turn images in to their emoji representation
@@ -30,7 +33,10 @@ impl Emoji {
     /// return euclidian distance from emoji's color
     /// to the supplied rgb value
     fn distance(&self, pixel: image::Rgb<u8>) -> f64 {
-       0.1 
+        let x : i32 = i32::pow(self.color.0 as i32 - pixel[0] as i32, 2);
+        let y : i32 = i32::pow(self.color.1 as i32 - pixel[1] as i32, 2);
+        let z : i32 = i32::pow(self.color.2 as i32 - pixel[2] as i32, 2);
+        f64::sqrt((x+y+z) as f64)
     }
 }
 
@@ -41,8 +47,26 @@ fn main() {
     let img = image::open(args.path).unwrap();
     let img: image::DynamicImage = img.thumbnail(args.size as u32, args.size as u32);
     let img: &image::RgbImage = img.as_rgb8().unwrap();
+    let (width, height) = img.dimensions();
 
-    println!("Pixel at (20,20) is {:?}", img[(20, 20)]);
+    //println!("Pixel at (20,20) is {:?}", img[(20, 20)]);
 
-    println!("{}", EMOJI[0].distance(img[(20, 20)]));
+    //println!("{}", EMOJI[0].distance(img[(20, 20)]));
+
+
+    for i in 0..height {
+        for j in 0..width {
+            let mut best_dist : f64 = f64::MAX; 
+            let mut best : Emoji = Emoji{color: (0,0,0), value: ' '};
+            for e in EMOJI {
+                let cur_dist :f64 = e.distance(img[(j,i)]);
+                if cur_dist < best_dist {
+                    best_dist = cur_dist;
+                    best = e;
+                }
+            }
+            print!("{}", best.value);
+        }
+        print!("\n")
+    }
 }
