f3cbc2e41f635d7bf81e47aa63bd6a7acdbc1926
Christoph Coijanovic <christoph.coijanovic@kit.edu>
2022-12-19 05:36:12 +0100

    Added cli flag parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 extern crate image;
+extern crate clap;
+
+use clap::Parser;
+
+/// Turn images in to their emoji representation
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to image
+    #[arg(short, long)]
+    path: String,
+
+    /// Size of image
+    #[arg(short, long, default_value_t = 25)]
+    size: u8,
+}
 
 fn main() {
-    
+    let args = Args::parse();
+
     // open and resize the image
-    let img = image::open("/home/christoph/Downloads/frog.jpg").unwrap();
-    let img: image::DynamicImage = img.thumbnail(100, 100);
+    let img = image::open(args.path).unwrap();
+    let img: image::DynamicImage = img.thumbnail(args.size as u32, args.size as u32);
     let img: &image::RgbImage = img.as_rgb8().unwrap();
 
     println!("Pixel at (20,20) is {:?}", img[(20, 20)]);
